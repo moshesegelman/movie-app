ec1a02fe6e77a839f674877be2f9f2964e6e8bef
moshesegelman <moshesegelman41@gmail.com>
2020-06-10 07:54:26 -0700

    adds assosiations

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,4 +3,5 @@ class Actor < ApplicationRecord
   validates :first_name, :last_name, length: {minimum: 2}
   validates :known_for, presence: true
   validates :age, presence: true, numericality: {greater_than_or_equal_to: 13}
+  belongs_to :movie
 end
