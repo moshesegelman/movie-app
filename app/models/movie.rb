2035f1050e4103ef592ae8dcdf7c4c3a471539ae
moshesegelman <moshesegelman41@gmail.com>
2020-06-15 19:29:23 -0400

    adds assosiations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,10 @@ class Movie < ApplicationRecord
   validates :plot, length: {minimum: 200}
   validates :director, presence: true
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
+
+  def genre_names
+    genres.map do {|genre| genre.name}
+  end
 end
