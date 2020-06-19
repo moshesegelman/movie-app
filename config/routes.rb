c90233081f1c585e570fd1db4fd4bcbfa6b80df9
moshesegelman <moshesegelman41@gmail.com>
2020-06-19 11:46:31 -0400

    adds front end show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
+  get "/movies" => "movies#show"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
